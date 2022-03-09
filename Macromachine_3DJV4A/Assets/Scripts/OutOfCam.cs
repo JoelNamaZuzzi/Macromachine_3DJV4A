a94c9da823ae62c78559350be304b46f064cd824
quent <quentin2000.fert@gmail.com>
2022-03-09 20:37:07 +0100

    test rotation

--- Macromachine_3DJV4A/Assets/Scripts/OutOfCam.cs
+++ Macromachine_3DJV4A/Assets/Scripts/OutOfCam.cs
@@ -10,6 +10,7 @@ public class OutOfCam : MonoBehaviour
         {
             Debug.Log("-1 Life");
             other.transform.position = RaceManager.Instance.Getfirstplaceplayer().transform.position;
+            other.transform.rotation = RaceManager.Instance.Getfirstplaceplayer().transform.rotation;
             // on donne au perdant les stats du premier joueur
             other.GetComponent<CarcpManager>().cpCrossed = RaceManager.Instance.Getfirstplaceplayer().GetComponent<CarcpManager>().cpCrossed;
             RaceManager.Instance.CarCollectedCp(other.GetComponent<CarcpManager>().CarNumber, other.GetComponent<CarcpManager>().cpCrossed);
