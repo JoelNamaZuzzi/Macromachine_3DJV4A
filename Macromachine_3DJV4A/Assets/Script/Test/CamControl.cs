8b3e4ed7e304188aaa8d0289d3713e45f696f90c
JoelNamaZuzzi <59820667+JoelNamaZuzzi@users.noreply.github.com>
2022-07-22 19:11:59 +0200

    Cam changes rot and pos when at checkpoint

--- Macromachine_3DJV4A/Assets/Script/Test/CamControl.cs
+++ Macromachine_3DJV4A/Assets/Script/Test/CamControl.cs
@@ -7,8 +7,11 @@ public class CamControl : MonoBehaviour
     [SerializeField] private Vector3 beginPos;
     [SerializeField] private Vector3 initRot;
     private Rigidbody rb;
+    public GameObject nextCheck;
+    public Vector3 nextPos;
     void Start()
     {
+        nextPos = nextCheck.transform.position;
         rb = GetComponent<Rigidbody>();
         transform.position = beginPos;
         transform.rotation = Quaternion.Euler(initRot);
@@ -17,5 +20,12 @@ public class CamControl : MonoBehaviour
     void FixedUpdate()
     {
         rb.velocity =transform.up *1.0f;
+        if (Vector3.Distance(nextPos, transform.position) <= 0.1f)
+        {
+            transform.position = nextCheck.transform.position;
+            transform.rotation = Quaternion.Euler(nextCheck.GetComponent<NextCheckPTS>().Rot);
+            nextCheck = nextCheck.GetComponent<NextCheckPTS>().nextCheck;
+            nextPos = nextCheck.transform.position;
+        }
     }
 }
---
+++ Macromachine_3DJV4A/Assets/Script/Test/NextCheckPTS.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextCheckPTS : MonoBehaviour
+{
+    public GameObject nextCheck;
+    public Vector3 Rot;
+}
